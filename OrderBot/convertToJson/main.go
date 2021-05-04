687e5288a104e282896a80537b27d0f002314ba9
PaulRiviera <PaulRiviera@outlook.com>
2021-05-04 16:41:58 -0400

    add crazy simple api for bot test

--- OrderBot/convertToJson/main.go
+++ OrderBot/convertToJson/main.go
@@ -3,27 +3,60 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
 
-type Row struct {
-	ShipmentNumber    string `json:"sd_ship_no,omitempty"`
-	ShipmentType      string `json:"sh_ship_typ_cd,omitempty"`
-	ShipToName        string `json:"sh_ship_to_nam,omitempty"`
-	CustomerProductID string `json:"sd_cust_prod_id,omitempty"`
-	ExtnDesc          string `json:"sd_extn_desc,omitempty"`
-	InvoiceNumber     string `json:"sd_invce_no,omitempty"`
-	DeliveryDate      string `json:"sd_prm_dlvry_dte,omitempty"`
-	OrderDate         string `json:"sh_ord_dte,omitempty"`
-	ShipDate          string `json:"sh_ship_dte,omitempty"`
-	ShipCode          string `json:"sh_ship_cd,omitempty"`
-	Product           string `json:"sd_prod_desc,omitempty"`
-	ProductID         string `json:"sd_prod_id,omitempty"`
+var (
+	ordersInMemory []Orders
+)
+
+func init() {
+	ordersInMemory = []Orders{}
+}
+
+func OrderStatusHandler(w http.ResponseWriter, r *http.Request) {
+	var req StatusRequest
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	ordersWithIDRequested := []Orders{}
+	deliveryDate := "soon"
+
+	for _, o := range ordersInMemory {
+		if o.ShipmentNumber == req.ID {
+			ordersWithIDRequested = append(ordersWithIDRequested, o)
+			deliveryDate = o.DeliveryDate
+		}
+	}
+
+	response := StatusResponse{}
+	response.Message = fmt.Sprintf("Order contains %v product(s) and will be delivered %v", len(ordersWithIDRequested), deliveryDate)
+
+	b, _ := json.Marshal(response)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(b)
+}
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("PING"))
 }
 
 func main() {
+	ordersInMemory = readInFile()
 
+	fmt.Println("Ready and Listening at Port 80")
+	http.HandleFunc("/ping", PingHandler)
+	http.HandleFunc("/order/status", OrderStatusHandler)
+	panic(http.ListenAndServe(":80", nil))
+}
+
+func readInFile() []Orders {
 	file, err := os.Open("./chatbot_data.local.csv")
 	if err != nil {
 		panic(err)
@@ -35,9 +68,8 @@ func main() {
 		panic(err)
 	}
 
-	rows := []Row{}
-	for _, r := range records[1:10] {
-
+	rows := []Orders{}
+	for _, r := range records {
 		newRow := convertRow(r)
 		rows = append(rows, *newRow)
 	}
@@ -56,10 +88,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	return rows
 }
 
-func convertRow(d []string) *Row {
-	r := Row{}
+func convertRow(d []string) *Orders {
+	r := Orders{}
 	r.ShipmentNumber = strings.Trim(d[0], " ")
 	r.ShipmentType = strings.Trim(d[1], " ")
 	r.ShipToName = strings.Trim(d[2], " ")
@@ -73,4 +107,27 @@ func convertRow(d []string) *Row {
 	r.Product = strings.Trim(d[10], " ")
 	r.ProductID = strings.Trim(d[11], " ")
 	return &r
+}
+
+type StatusResponse struct {
+	Message string `json:"message"`
+}
+
+type StatusRequest struct {
+	ID string `json:"id"`
+}
+
+type Orders struct {
+	ShipmentNumber    string `json:"sd_ship_no,omitempty"`
+	ShipmentType      string `json:"sh_ship_typ_cd,omitempty"`
+	ShipToName        string `json:"sh_ship_to_nam,omitempty"`
+	CustomerProductID string `json:"sd_cust_prod_id,omitempty"`
+	ExtnDesc          string `json:"sd_extn_desc,omitempty"`
+	InvoiceNumber     string `json:"sd_invce_no,omitempty"`
+	DeliveryDate      string `json:"sd_prm_dlvry_dte,omitempty"`
+	OrderDate         string `json:"sh_ord_dte,omitempty"`
+	ShipDate          string `json:"sh_ship_dte,omitempty"`
+	ShipCode          string `json:"sh_ship_cd,omitempty"`
+	Product           string `json:"sd_prod_desc,omitempty"`
+	ProductID         string `json:"sd_prod_id,omitempty"`
 }
